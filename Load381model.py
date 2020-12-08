4712b19d6fab69a757d5633bbb923dddb6cc2b05
Abu3li <hossainalyousif@gmail.com>
2020-12-08 19:54:24 +0300

    Added yasser files organized some of the files

--- Load381model.py
+++ Load381model.py
@@ -1,12 +1,14 @@
 from keras.models import load_model
 from adabelief_tf import AdaBeliefOptimizer
+from tensorflow.keras.layers import Layer, InputSpec
+from tensorflow.python.keras.layers import Layer, InputSpec
 
 
 def loadMobileNetModel():
-    name = load_model('./mask_models/mobileNetModel381.h5')
+    name = load_model('mask_models/saved_models/mobileNetModel381.h5')
     return name
 
 
 def loadOurOwnModel():
-    name = load_model('./mask_models/ourOwnModel381fixed.h5')
+    name = load_model('mask_models/saved_models/ourOwnModel381fixed.h5')
     return name
